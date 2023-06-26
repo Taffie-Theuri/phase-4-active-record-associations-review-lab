bf158d5739889c212e00c5f67933d29951ba708d
Taffie Theuri <taffietheuri4@gmail.com>
2023-06-26 15:39:59 +0300

    Completed

--- db/migrate/20210506153127_create_rides.rb
+++ db/migrate/20210506153127_create_rides.rb
@@ -2,7 +2,15 @@ class CreateRides < ActiveRecord::Migration[6.1]
   def change
     create_table :rides do |t|
 
+      t.belongs_to :taxi
+      t.belongs_to :passenger
+
       t.timestamps
     end
   end
 end
+
+#Also correct
+#t.integer "passenger_id"
+#t.integer "taxi_id"
+
